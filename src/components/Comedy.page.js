e562e7afe46bd9dac4329b3ab041a5b5bdbd9b00
Harish T M <harishtm77@gmail.com>
2024-08-31 17:08:59 +0530

    useQuery second argument queryFunction enhancement

--- src/components/Comedy.page.js
+++ src/components/Comedy.page.js
@@ -1,11 +1,13 @@
 import axios from "axios"
 import { useQuery } from "react-query"
 
+const fetchComedyMovies = () => {
+    return axios.get('http://localhost:4000/comedy')
+}
+
 export const Comedy = () => {
 
-    const { isLoading, data} = useQuery('comedy-movies', () => {
-        return axios.get('http://localhost:4000/comedy')
-    })
+    const { isLoading, data} = useQuery('comedy-movies', fetchComedyMovies)
 
     if(isLoading) {
         return <div>Loading...</div>
