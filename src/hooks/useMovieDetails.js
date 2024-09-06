751b641906458bf406da086aef3fb83c51ec3003
Harish T M <harishtm77@gmail.com>
2024-09-06 09:44:42 +0530

    understanding initialData config property

--- src/hooks/useMovieDetails.js
+++ src/hooks/useMovieDetails.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 
 
 const fetchMovieDetails = (movieId) => {
@@ -7,8 +7,22 @@ const fetchMovieDetails = (movieId) => {
 }
 
 export const useMovieDeatails = (movieId) => {
+    const queryClient = useQueryClient()
     return useQuery(
         ['movie-details', movieId],
-        () => fetchMovieDetails(movieId)
+        () => fetchMovieDetails(movieId),
+        {
+            initialData: () => {
+                const movie = queryClient
+                .getQueriesData('fantasy-movies')
+                ?.data?.find((movie) => parseInt(movieId) === parseInt(movie.id))
+
+                if(movie) {
+                    return { data: movie}
+                } else {
+                    return undefined
+                }
+            }
+        }
     )
 }
